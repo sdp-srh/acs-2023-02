bce59a3d7d8893d0bd708aa134cd92214c9f893a
sdp-srh <105343612+sdp-srh@users.noreply.github.com>
2023-04-18 16:36:24 +0200

    included firstore

--- soccer-functions/index.js
+++ soccer-functions/index.js
@@ -1,25 +1,31 @@
 const functions = require('@google-cloud/functions-framework');
 const axios = require('axios')
+const { Firestore } = require('@google-cloud/firestore')
+
 
 // TODO get from environment or db
 const league = 'bl1'
 const season = '2022'
 
 functions.http('load-openliga-data', async (req, res) => {
+  const firestore = new Firestore()
+
   console.log('load team data called')
   const teamResponse = await axios.get(`https://www.openligadb.de/api/getavailableteams/${league}/${season}`)
   const teams = teamResponse.data
+  const teamsCollection = await firestore.collection(`teams`)
+
   for (const team of teams) {
-    // TODO store the teams data in nosql database (firestore)
-    // console.log(team)
+    await teamsCollection.doc(''+team.TeamId).set(team)
   }
   const matchResponse = await  axios.get(`https://www.openligadb.de/api/getmatchdata/${league}/${season}`)
   const matches = matchResponse.data
-  let message = `Loaded matches ${matches.length} from OpenLigaDB`
+  const matchCollection = await firestore.collection(`matches`)
+ 
   for (const match of matches) {
-    // TODO store in firestore
-    // console.log(matches)
+    await matchCollection.doc(''+match.MatchID).set(match)
   }
-  console.log(`matches (${matches.length}), teams (${teams.length})`)
+  let message = `Loaded matches (${matches.length}), teams (${teams.length})`
+  console.log(message)
   res.send({status:'OK', message: message })
 })
